c29e92ef59576641a2ac534a7da7a1dcd6695fb6
Kike <enrique.gvalls@gmail.com>
2021-01-31 18:13:43 +0100

    fix intro text

--- src/components/IntroText/IntroText.js
+++ src/components/IntroText/IntroText.js
@@ -26,20 +26,20 @@ max-width: 75rem;
 `;
 
 const Flexed = styled.div`
-display: flex;
-justify-content: center;
-align-items: baseline;
 margin-top: 2rem;
 margin-bottom: 4rem;
+width: 80%;
+max-width: 75rem;
 `;
 
 const Text2 = styled.p`
 color: ${props => props.theme.colors.text1};
 font-size: ${props => props.theme.fontSizes.titleExtraSmall};
 font-weight: bold;
+text-align: center;
 `;
 
-const Text3 = styled.p`
+const Text3 = styled.span`
 color: ${props => props.theme.colors.blue3};
 font-size: ${props => props.theme.fontSizes.titleExtraSmall};
 font-weight: bold;
@@ -52,8 +52,7 @@ const IntroText = ({ intro }) => {
             <Text1>{intro && intro[0].text1}</Text1>
             <Text1a>{intro && intro[0].text2}</Text1a>
             <Flexed>
-                <Text2>{intro && intro[0].quote1}</Text2> &nbsp;&nbsp;
-                <Text3>{intro && intro[0].quote2}</Text3>
+                <Text2>{intro && intro[0].quote1} <Text3>{intro && intro[0].quote2}</Text3></Text2>  
             </Flexed>
         </DescriptionTextWrapper>
     )
